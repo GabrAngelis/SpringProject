09fa387a873bc20a9ac19bc8385e71b815fadc30
GabrAngelis <angelisgabrielluisdeangel@gmail.com>
2024-10-01 16:34:54 -0300

    Primeiro commit

---
+++ src/test/java/spring/bot/project/gabriel/project/GabrielProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.bot.project.gabriel.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GabrielProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
