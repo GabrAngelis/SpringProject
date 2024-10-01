9e9d0602ae08b5b8b2721d23015e214a7258839d
GabrAngelis <angelisgabrielluisdeangel@gmail.com>
2024-10-01 16:34:54 -0300

    Primeiro commit

---
+++ src/main/java/spring/bot/project/gabriel/project/GabrielProjectApplication.java
@@ -0,0 +1,13 @@
+package spring.bot.project.gabriel.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GabrielProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GabrielProjectApplication.class, args);
+	}
+
+}
